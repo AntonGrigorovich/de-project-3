d61d1ae9377d86ee4f31254307bba1ffa2f30fa0
AntonGrigorovich <110724187+AntonGrigorovich@users.noreply.github.com>
2022-09-15 23:28:44 +0300

    Add files via upload

--- src/DDL.sql
+++ src/DDL.sql
@@ -1,4 +1,4 @@
-ALTER TABLE staging.user_order_log ADD COLUMN status varchar(15) 
+ALTER TABLE staging.user_order_log ADD COLUMN status varchar(15) NOT NULL Default 'shipped'
 
 ALTER TABLE mart.f_sales ADD COLUMN status varchar(15) NOT NULL Default 'shipped'
 
